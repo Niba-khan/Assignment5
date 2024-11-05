285f9928916e3c7eb96739b794f32549ec460903
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2024-11-05 16:14:56 +0500

    Assignment

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
         <div className="bg-[#A29875] h-[134px] flex justify-center items-center">
            <div className="absolute top-[20px] left-[36px] p-5">
         <Image
-          src="/image/logo1.png" 
+          src={"/image/logo1.png"}
           alt="Logo"
           width={439}
           height={94}
